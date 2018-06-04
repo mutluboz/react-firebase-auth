4552c2fce936b83d1c77a9bb2b2962d51fc148fa
Mut <mutlu_boz@outlook.com>
2018-06-04 14:23:45 +0300

    initial commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+  apiKey: 'AIzaSyDA_QB-wrkM2Rp2ssrvNYBz-A1QUEQCo58',
+  authDomain: 'fir-authdemo-44294.firebaseapp.com',
+  databaseURL: 'https://fir-authdemo-44294.firebaseio.com',
+  projectId: 'fir-authdemo-44294',
+  storageBucket: 'fir-authdemo-44294.appspot.com',
+  messagingSenderId: '1090377600103'
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export { auth };
